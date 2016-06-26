b7dc45205fe7720afc16f0d284b1f62fc282a444
Saad Ali <agentsaad007@yahoo.com>
2016-06-26 18:48:55 +0500

    Accidently uploaded the wrong file earlier, completely working code.

--- Module 2/client.py
+++ Module 2/client.py
@@ -1,28 +1,27 @@
 
-
-import socket               
+import socket
 from thread import *
 
 s = socket.socket()         
 host = socket.gethostname()
-port = 5188                
+port = 6188
 s.connect((host, port))
-x=raw_input("Enter Your Name : ")
+x= raw_input("Enter Your Name : ")
 s.send(x)
-n=raw_input("You want connection with : ")
-s.send(n)
+
 
 def func(s):
-	while 1:
-		x=s.recv(1024)
-		print x
+    while 1:
+        x = s.recv(1024)
+        print x
 
-start_new_thread(func,(s,))
+
+start_new_thread(func, (s,))
 while 1:
-	x=raw_input()	
-	if x=='quit()':
-		s.send(x)
-		break
-	else:
-		s.send(x)
-s.close()             
+    x = raw_input()
+    if x == 'quit()':
+        s.send(x)
+        break
+    else:
+        s.send(x)
+s.close()
